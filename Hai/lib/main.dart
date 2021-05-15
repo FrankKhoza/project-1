9484903f1293b20708f8c7f102b38d4a4dbca8dc
Frank Khoza <201601976@ub.ac.bw>
2021-05-15 19:13:18 +0200

    First commit

---
+++ Hai/lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/haiapp/generatedloginframewidget/GeneratedLoginframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedcovid19alertsframewidget/GeneratedCovid19alertsframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedresultsframewidget/GeneratedResultsframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedsignupframewidget/GeneratedSignUpframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedpasswordresetframewidget/GeneratedPasswordResetframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedenteremailframewidget/GeneratedEnteremailframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedverificationcodeframewidget/GeneratedVerificationCodeframeWidget.dart';
+import 'package:flutterapp/haiapp/generateddeleteaccountframewidget/GeneratedDeleteAccountframeWidget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax17widget/GeneratedIPhone11ProMax17Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax20widget/GeneratedIPhone11ProMax20Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax19widget/GeneratedIPhone11ProMax19Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax15widget/GeneratedIPhone11ProMax15Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax14widget/GeneratedIPhone11ProMax14Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax13widget/GeneratedIPhone11ProMax13Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax12widget/GeneratedIPhone11ProMax12Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax11widget/GeneratedIPhone11ProMax11Widget.dart';
+import 'package:flutterapp/haiapp/generatediphone11promax5widget/GeneratedIPhone11ProMax5Widget.dart';
+import 'package:flutterapp/haiapp/generatedmessagesframewidget/GeneratedMessagesframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedfilterframe3widget/GeneratedFilterframe3Widget.dart';
+import 'package:flutterapp/haiapp/generatedfeelingsickframewidget/GeneratedFeelingSickframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedvideocallubframewidget/GeneratedVideocallUBframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedcallingubframewidget/GeneratedCallingUBframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedfeedframewidget/GeneratedFeedframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedupdatesframewidget/GeneratedUpdatesframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedhomeframewidget/GeneratedHomeframeWidget.dart';
+import 'package:flutterapp/haiapp/generatededitprofileframewidget/GeneratedEditProfileframeWidget.dart';
+import 'package:flutterapp/haiapp/generatedeventsframwidget/GeneratedEventsframWidget.dart';
+import 'package:flutterapp/haiapp/generatedsettingsframewidget/GeneratedSettingsframeWidget.dart';
+import 'package:flutterapp/haiapp/generatediosbottombar5tabswidget5/GeneratedIOSBottomBar5TabsWidget5.dart';
+import 'package:flutterapp/haiapp/generatedfilterframe2widget/GeneratedFilterframe2Widget.dart';
+import 'package:flutterapp/haiapp/generatedfilterframe1widget/GeneratedFilterframe1Widget.dart';
+
+void main() {
+  runApp(HaiApp());
+}
+
+class HaiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedLoginframeWidget',
+      routes: {
+        '/GeneratedLoginframeWidget': (context) => GeneratedLoginframeWidget(),
+        '/GeneratedCovid19alertsframeWidget': (context) =>
+            GeneratedCovid19alertsframeWidget(),
+        '/GeneratedResultsframeWidget': (context) =>
+            GeneratedResultsframeWidget(),
+        '/GeneratedSignUpframeWidget': (context) =>
+            GeneratedSignUpframeWidget(),
+        '/GeneratedPasswordResetframeWidget': (context) =>
+            GeneratedPasswordResetframeWidget(),
+        '/GeneratedEnteremailframeWidget': (context) =>
+            GeneratedEnteremailframeWidget(),
+        '/GeneratedVerificationCodeframeWidget': (context) =>
+            GeneratedVerificationCodeframeWidget(),
+        '/GeneratedDeleteAccountframeWidget': (context) =>
+            GeneratedDeleteAccountframeWidget(),
+        '/GeneratedIPhone11ProMax17Widget': (context) =>
+            GeneratedIPhone11ProMax17Widget(),
+        '/GeneratedIPhone11ProMax20Widget': (context) =>
+            GeneratedIPhone11ProMax20Widget(),
+        '/GeneratedIPhone11ProMax19Widget': (context) =>
+            GeneratedIPhone11ProMax19Widget(),
+        '/GeneratedIPhone11ProMax15Widget': (context) =>
+            GeneratedIPhone11ProMax15Widget(),
+        '/GeneratedIPhone11ProMax14Widget': (context) =>
+            GeneratedIPhone11ProMax14Widget(),
+        '/GeneratedIPhone11ProMax13Widget': (context) =>
+            GeneratedIPhone11ProMax13Widget(),
+        '/GeneratedIPhone11ProMax12Widget': (context) =>
+            GeneratedIPhone11ProMax12Widget(),
+        '/GeneratedIPhone11ProMax11Widget': (context) =>
+            GeneratedIPhone11ProMax11Widget(),
+        '/GeneratedIPhone11ProMax5Widget': (context) =>
+            GeneratedIPhone11ProMax5Widget(),
+        '/GeneratedMessagesframeWidget': (context) =>
+            GeneratedMessagesframeWidget(),
+        '/GeneratedFilterframe3Widget': (context) =>
+            GeneratedFilterframe3Widget(),
+        '/GeneratedFeelingSickframeWidget': (context) =>
+            GeneratedFeelingSickframeWidget(),
+        '/GeneratedVideocallUBframeWidget': (context) =>
+            GeneratedVideocallUBframeWidget(),
+        '/GeneratedCallingUBframeWidget': (context) =>
+            GeneratedCallingUBframeWidget(),
+        '/GeneratedFeedframeWidget': (context) => GeneratedFeedframeWidget(),
+        '/GeneratedUpdatesframeWidget': (context) =>
+            GeneratedUpdatesframeWidget(),
+        '/GeneratedHomeframeWidget': (context) => GeneratedHomeframeWidget(),
+        '/GeneratedEditProfileframeWidget': (context) =>
+            GeneratedEditProfileframeWidget(),
+        '/GeneratedEventsframWidget': (context) => GeneratedEventsframWidget(),
+        '/GeneratedSettingsframeWidget': (context) =>
+            GeneratedSettingsframeWidget(),
+        '/GeneratedIOSBottomBar5TabsWidget5': (context) =>
+            GeneratedIOSBottomBar5TabsWidget5(),
+        '/GeneratedFilterframe2Widget': (context) =>
+            GeneratedFilterframe2Widget(),
+        '/GeneratedFilterframe1Widget': (context) =>
+            GeneratedFilterframe1Widget(),
+      },
+    );
+  }
+}
